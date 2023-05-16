02a86a5c84a41074bcfe5a752c4f9ef073c889a8
Jasmine Aikey <jaaikey1@gmail.com>
2023-05-16 14:04:15 -0700

    dded makefile

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -4,7 +4,9 @@ from calculator_adapter import run
 
 ### ADD AT LEAST TWO TESTS HERE!
 
-
-###
+# Checks that the program outputs "20" for an input of "4 * 5"
+assert run("4 * 5").output == "20"
+# Checks that the program fails (correctly errors) for input "5 ^ 6"
+assert run("5 ^ 6").exit_status != 0
 
 print("All tests passed!")
